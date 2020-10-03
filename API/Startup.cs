724031b103ce2d1a9b5aaaa8206799748c11f296
Bennet Dumashie <bexoda2000@gmail.com>
2020-10-03 10:53:43 +0000

    Added clients side angular app section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,12 @@ namespace API
 
             app.UseHttpsRedirection();
 
+            
+
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
